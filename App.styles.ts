861e25007a4e5a615af84cdcffa89fdb9a53b6a2
Anton Shevchenko <anton950712@mail.ru>
2023-04-18 20:17:42 +0200

    feat: Update header navigation

--- App.styles.ts
+++ App.styles.ts
@@ -1,25 +1,9 @@
 import { StyleSheet } from "react-native";
 
-import { colors } from "Constants";
-
 export const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    marginBottom: 20
-  },
-  header: {
-    backgroundColor: colors.blue500,
-    padding: 16,
-    borderBottomColor: colors.gray,
-    borderBottomWidth: 1
-  },
-  headerText: {
-    fontFamily: "Roboto",
-    fontSize: 20,
-    fontWeight: "500",
-    lineHeight: 24,
-    textAlign: "center",
-    letterSpacing: 0.15,
-    color: colors.white
+    marginBottom: 20,
+    fontFamily: "Roboto"
   }
 });
--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { SafeAreaView, StatusBar, Text, View } from "react-native";
+import { Button, SafeAreaView, StatusBar, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { ProductRoute } from "Routes/Product";
 import { HomeRoute } from "Routes/Home";
 import { Routes } from "Types/routes.interface";
+import { colors } from "Constants";
 
 import { styles } from "./App.styles";
 
@@ -17,12 +18,40 @@ const App = () => {
     <NavigationContainer>
       <SafeAreaView style={styles.safeArea}>
         <StatusBar animated />
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Ecommerce Store</Text>
-        </View>
-        <Stack.Navigator>
-          <Stack.Screen name={Routes.Home} component={HomeRoute} />
-          <Stack.Screen name={Routes.Product} component={ProductRoute} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: colors.blue500
+            },
+            headerTintColor: colors.white,
+            headerTitleStyle: {
+              fontWeight: "500",
+              fontFamily: "Roboto",
+              fontSize: 20
+            },
+            headerTitleAlign: "center",
+            headerRight: () => (
+              <Button title="O" />
+            )
+          }}
+        >
+          <Stack.Screen
+            name={Routes.Home}
+            component={HomeRoute}
+            options={{
+              title: "Ecommerce Store",
+              headerLeft: () => (
+                <Button title="=" />
+              )
+            }}
+          />
+          <Stack.Screen
+            name={Routes.Product}
+            component={ProductRoute}
+            options={{
+              title: ""
+            }}
+          />
         </Stack.Navigator>
       </SafeAreaView>
     </NavigationContainer>
